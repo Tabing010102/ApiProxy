fa3f2a8744c0ec171b81a0f73de5b53cfb47bebf
Tabing010102 <tabing010102@outlook.com>
2024-12-08 16:10:34 +0800

    feat: add opencc converter for ApiProxy-Python

--- ApiProxy-Python/app.py
+++ ApiProxy-Python/app.py
@@ -1,8 +1,10 @@
 import argparse
 import json
+import logging
 import threading
 from itertools import cycle
 
+import opencc
 import requests
 from flask import Flask, request
 
@@ -10,7 +12,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-l', '--listen_host', default='127.0.0.1', help='Host to listen on')
 parser.add_argument('-p', '--listen_port', default=8081, type=int, help='Port to listen on')
 parser.add_argument('-c', '--config', default='config.json', help='Config file')
+parser.add_argument('-d', '--debug', action='store_true', help='Enable debug mode')
 args = parser.parse_args()
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG)
 
 with open(args.config, 'r') as f:
     config = json.load(f)
@@ -18,8 +24,13 @@ with open(args.config, 'r') as f:
 endpoints = [{'endpoint': c['endpoint'],
               'semaphore': threading.Semaphore(c['max_concurrency']),
               'timeout': c['timeout']}
-             for c in config]
+             for c in config['endpoints']]
 endpoints_cycle = cycle(endpoints)
+
+opencc_enabled = bool(config['enable_opencc'])
+opencc_converter: opencc.OpenCC
+if opencc_enabled:
+    opencc_converter = opencc.OpenCC(config['opencc_config'])
 
 app = Flask(__name__)
 
@@ -41,8 +52,23 @@ def forward_request(request, endpoint):
             cookies=request.cookies,
             timeout=endpoint['timeout'],
             allow_redirects=False)
+        encoding = r.encoding if r.encoding else 'utf-8'
+        app.logger.debug(f'Received response: {r.content.decode(encoding)}')
 
-        return r.content, r.status_code, r.raw.headers.items()
+        if opencc_enabled:
+            if opencc_enabled:
+                json_ensure_ascii = False if (encoding == 'utf-8' or encoding == 'utf8') else True
+                response_text = r.content.decode(encoding)
+                response_json = json.loads(response_text)
+                for choice in response_json.get('choices', []):
+                    if 'message' in choice and 'content' in choice['message']:
+                        choice['message']['content'] = opencc_converter.convert(choice['message']['content'])
+                response_text = json.dumps(response_json, ensure_ascii=json_ensure_ascii, separators=(',', ':'))
+                response_content = response_text.encode(encoding)
+                app.logger.debug(f'Converted response: {response_text}')
+                return response_content, r.status_code, r.raw.headers.items()
+        else:
+            return r.content, r.status_code, r.raw.headers.items()
     except Exception as e:
         return str(e), 500
 
